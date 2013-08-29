32624fccecb0c914851678ee1a254125aca0a0d4
Jakub Pastuszek <jpastuszek@gmail.com>
2013-08-29 14:21:21 +0200

    structured reducer

--- lib/quickmr/reducer.rb
+++ lib/quickmr/reducer.rb
@@ -2,11 +2,11 @@ require 'quickmr/processor_base'
 
 class Reducer < ProcessorBase
 	@@reducer_no = 0
-	def self.define(name = "Reducer#{@@reducer_no += 1}", &record_processor)
+	def self.define(name = "Reducer#{@@reducer_no += 1}", &setup)
 		reducer = Class.new(Reducer) do
-			@@record_processor = record_processor
-			def record_processor
-				@@record_processor
+			@@setup = setup
+			def setup
+				instance_exec(&@@setup)
 			end
 		end
 		Object.const_set(name, reducer)
@@ -30,17 +30,33 @@ class Reducer < ProcessorBase
 	def initialize(options)
 		super
 		@collector = Collector.new(self)
+		setup
+
+		@collector.instance_exec(&@on_start) if @on_start
 	end
 
 private
 
+	def start(&block)
+		@on_start = block
+	end
+
+	def each(&block)
+		@on_each = block
+	end
+
+	def finish(&block)
+		@on_finish = block
+	end
+
 	def on_data(event)
 		if not event.data
 			shutdown!
+			@collector.instance_exec(&@on_finish) if @on_finish
 			@collector.flush!
 			return
 		end
-		@collector.instance_exec(*event.data, &record_processor)
+		@collector.instance_exec(*event.data, &@on_each) if @on_each
 	end
 end
 
--- spec/reducer_spec.rb
+++ spec/reducer_spec.rb
@@ -3,39 +3,54 @@ require 'quickmr/reducer'
 
 describe Reducer do
 	let :data do
-		[[1, 1], [1, 2], [2, 1], [2, 2], [3, 1], [3, 2], [4, 1], [4, 2], [5, 1], [5, 2], [6, 1], [6, 2], [7, 1], [7, 2], nil]
+		# modulo and it's base
+		(0...20).map do |no|
+			[(no % 10), no]
+		end.sort_by do |key, value|
+			"%s#%10s" % [key, value]
+		end | [nil]
 	end
 
-	subject do
-		Reducer.define do |key, value|
-			@last_key = nil unless defined? @last_key
-			@sum = 0 unless defined? @sum
+	describe 'basic reducer structure' do
+		subject do
+			# sum base for each modulo
+			Reducer.define do |key, value|
+				start do
+					@last_key = nil
+					@sum = 0
+				end
 
-			if key != @last_key
-				collect(@last_key, @sum) if @last_key
-				@sum = 0
-				@last_key = key
+				each do |key, value|
+					if key != @last_key
+						collect(@last_key, @sum) if @last_key
+						@sum = 0
+						@last_key = key
+					end
+					@sum += value
+				end
+
+				finish do
+					collect(@last_key, @sum)
+				end
 			end
-
-			@sum += value
 		end
-	end
 
-	it 'should provide key value records from sorted input records' do
-		processor = Class.new
-		processor_messages = []
-		processor.stub(:deliver_message!) {|*args| processor_messages << args}
+		it 'should provide key value records from sorted input records' do
+			processor = Class.new
+			processor_messages = []
+			processor.stub(:deliver_message!) {|*args| processor_messages << args}
 
-		reducer = Tribe.root.spawn(subject)
-		reducer.connect(processor)
+			reducer = Tribe.root.spawn(subject)
+			reducer.connect(processor)
 
-		data.each do |pair|
-			reducer.deliver_message! :data, pair
+			data.each do |pair|
+				reducer.deliver_message! :data, pair
+			end
+			
+			while reducer.alive? do sleep 0.1 end
+
+			processor_messages.should == [[:data, [0, 10]], [:data, [1, 12]], [:data, [2, 14]], [:data, [3, 16]], [:data, [4, 18]], [:data, [5, 20]], [:data, [6, 22]], [:data, [7, 24]], [:data, [8, 26]], [:data, [9, 28]], [:data, nil]]
 		end
-		
-		while reducer.alive? do sleep 0.1 end
-
-		processor_messages.should == [[:data, [1, 3]], [:data, [2, 3]], [:data, [3, 3]], [:data, [4, 3]], [:data, [5, 3]], [:data, [6, 3]], [:data, nil]]
 	end
 end
 
